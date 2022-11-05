d8f96d19ccd18c71d8cf5eac3be04aeebfbdf277
Chandan <chandancvp08@gmail.com>
2022-11-06 01:49:35 +0530

    updated the next-auth secret

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,8 +3,7 @@ import GoogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
   // Configure one or more authentication providers
-  secret: "Qx8Ld/zOhtfDA1iTnqYkHELe5FdLwhJ3Yy39imc2p5E=",
-  providers: [
+    providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID?process.env.GOOGLE_ID:"",
       clientSecret: process.env.GOOGLE_SECRET?process.env.GOOGLE_SECRET:"",
