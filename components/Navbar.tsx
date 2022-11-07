ef65cdd7b73f326f2c4f8002d5ea675316db7ab9
Neat11 <hello.astitva@gmail.com>
2022-11-08 01:20:37 +0530

    HomePage UI

---
+++ components/Navbar.tsx
@@ -0,0 +1,30 @@
+import { AppBar, Button, Toolbar } from '@mui/material'
+import { signIn, signOut, useSession } from 'next-auth/react';
+import React from 'react'
+import styles from '../styles/Home.module.css';
+
+function Navbar() {
+    const{data:session} = useSession();
+  return (
+    <AppBar>
+    <Toolbar className={styles.appbar}>
+      <div className='flex flex-row justify-between items-center w-full'>
+        <p>Falak 22</p>
+        <div className={styles.appBarShit}>
+          <Button variant='contained' className='rounded-full'>HOME</Button>
+          <Button variant='contained' className='rounded-full'>EVENTS</Button>
+          <Button variant='contained' className='rounded-full'>SCHEDULE</Button>
+          {session?
+            <Button variant='contained' className='rounded-full' color='secondary' onClick={()=>{signIn()}}>sign in</Button>
+          :
+            <Button className='rounded-full' variant='contained' color='secondary' onClick={()=>{signOut()}}>sign out</Button>
+          }
+        </div>
+      </div>
+    </Toolbar>
+  </AppBar>
+
+  )
+}
+
+export default Navbar
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,53 @@
-import { Button } from '@mui/material'
+import { AppBar, Button, createTheme, LinearProgress, ThemeProvider, Toolbar } from '@mui/material'
 import { signIn, signOut, useSession } from 'next-auth/react'
-
+import Navbar from '../components/Navbar';
+import Placard from '../components/placard';
+import Tile from '../components/Tile';
+import styles from '../styles/Home.module.css'
 export default function Home() {
-  const {data:session} = useSession();
-if(session){
-return(
-  <div>
-    <Button onClick={()=>{signOut()}}>Sign out</Button>
-    <p>
-      {JSON.stringify(session.user)}
-    </p>
-    <img src={session.user?.image!=null?session.user.image:undefined}/>
-  </div>
-)
-}
-else{
+  const{data:session} = useSession();
+  const theme = createTheme({
+    palette:{
+      primary:{
+        main:"#6C72D9",
+        
+      },
+      secondary:{
+        main:"#FFFFFF"
+      }
+    }
+  })
+  return(
+  <ThemeProvider theme={theme}>
 
-  return (
-    <div>
-        <Button onClick={()=>{signIn()}}>
-          sign in
-        </Button>
+  <div className={styles.container}>
+   <Navbar/>
+    <div className={styles.logo_bg}>
+      <img src=''/>
+    </div>
+    <div className="flex justify-center items-center">
+      <div className={styles.about_us}>
+        <p className=' text-center text-5xl font-bold text-white'>About Us</p>
+        <div className={styles.underline}/>
+        <div className='flex flex-row items-center justify-center mt-10'>
+        <p className='flex flex-end content-center text-center text-xl text-white'>lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum </p>
+        </div>
       </div>
-  )
-}
+    </div>
+    <div className='flex justify-center p-20'>
+      <video className=' bg-slate-600 w-10/12 rounded'></video>
+    </div>
+    <div className={styles.events}>
+      <p className='text-center text-5xl font-bold text-white'>Events</p>
+      <div className={styles.underline2}/>
+      <div className={styles.grid_container}>
+        <Tile src='https://images.pexels.com/photos/414102/pexels-photo-414102.jpeg?cs=srgb&dl=pexels-pixabay-414102.jpg&fm=jpg' name='Event Name' date='Dec 25' description='Lal Chadi Santa, raat ko bajayega ghanta' pass='platinum'/>
+        <Tile src='https://images.pexels.com/photos/414102/pexels-photo-414102.jpeg?cs=srgb&dl=pexels-pixabay-414102.jpg&fm=jpg' name='Event Name' date='Dec 25' description='Lal Chadi Santa, raat ko bajayega ghanta' pass='platinum'/>
+        <Tile src='https://images.pexels.com/photos/414102/pexels-photo-414102.jpeg?cs=srgb&dl=pexels-pixabay-414102.jpg&fm=jpg' name='Event Name' date='Dec 25' description='Lal Chadi Santa, raat ko bajayega ghanta' pass='platinum'/>
+        <Tile src='https://images.pexels.com/photos/414102/pexels-photo-414102.jpeg?cs=srgb&dl=pexels-pixabay-414102.jpg&fm=jpg' name='Event Name' date='Dec 25' description='Lal Chadi Santa, raat ko bajayega ghanta' pass='platinum'/>
+        </div>
+    </div>
+  </div>
+  </ThemeProvider>
+)
 } 
