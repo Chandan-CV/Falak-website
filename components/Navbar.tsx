90dc441fac87b8de084ad44f4de5872cc04cc7b3
Neat11 <hello.astitva@gmail.com>
2022-11-08 22:23:55 +0530

    mandatory merge commit

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,19 @@
 import { AppBar, Button, Toolbar } from '@mui/material'
 import { signIn, signOut, useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
 import React from 'react'
 import styles from '../styles/Home.module.css';
 
 function Navbar() {
     const{data:session} = useSession();
+    const router = useRouter();
   return (
     <AppBar>
     <Toolbar className={styles.appbar}>
       <div className='flex flex-row justify-between items-center w-full'>
         <p>Falak 22</p>
         <div className={styles.appBarShit}>
-          <Button variant='contained' className='rounded-full'>HOME</Button>
+          <Button variant='contained' className='rounded-full' onClick={()=>{router.replace('/')}}>HOME</Button>
           <Button variant='contained' className='rounded-full'>EVENTS</Button>
           <Button variant='contained' className='rounded-full'>SCHEDULE</Button>
           {session?
--- pages/index.tsx
+++ pages/index.tsx
@@ -38,14 +38,23 @@ export default function Home() {
       <video className=' bg-slate-600 w-10/12 rounded'></video>
     </div>
     <div className={styles.events}>
-      <p className='text-center text-5xl font-bold text-white'>Events</p>
+      <p className='text-center text-5xl font-bold text-white mt-10'>Events</p>
       <div className={styles.underline2}/>
       <div className={styles.grid_container}>
         <Tile src='https://images.pexels.com/photos/414102/pexels-photo-414102.jpeg?cs=srgb&dl=pexels-pixabay-414102.jpg&fm=jpg' name='Event Name' date='Dec 25' description='Lal Chadi Santa, raat ko bajayega ghanta' pass='Bronze'/>
         <Tile src='https://images.pexels.com/photos/414102/pexels-photo-414102.jpeg?cs=srgb&dl=pexels-pixabay-414102.jpg&fm=jpg' name='Event Name' date='Dec 25' description='Lal Chadi Santa, raat ko bajayega ghanta' pass='Silver'/>
         <Tile src='https://images.pexels.com/photos/414102/pexels-photo-414102.jpeg?cs=srgb&dl=pexels-pixabay-414102.jpg&fm=jpg' name='Event Name' date='Dec 25' description='Lal Chadi Santa, raat ko bajayega ghanta' pass='Gold'/>
         <Tile src='https://images.pexels.com/photos/414102/pexels-photo-414102.jpeg?cs=srgb&dl=pexels-pixabay-414102.jpg&fm=jpg' name='Event Name' date='Dec 25' description='Lal Chadi Santa, raat ko bajayega ghanta' pass='Platinum'/>
+      </div>
+      <div>
+        <div className='flex flex-col items-center'>
+      <p className=' text-5xl font-bold text-white mt-36'>The Team</p>
+      <div className={styles.underline3}/>
         </div>
+      <div className={styles.grid_container}>
+        <Placard name='Chandu' src='https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8cmFuZG9tJTIwcGVyc29ufGVufDB8fDB8fA%3D%3D&w=1000&q=80' year='1' pos='Senior Developer'/>
+      </div>
+      </div>
     </div>
   </div>
   </ThemeProvider>
