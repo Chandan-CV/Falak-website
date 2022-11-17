e4dbd501dde043861bd73c226535a89971cde698
Chandan <chandancvp08@gmail.com>
2022-11-17 19:15:11 +0530

    registrations for outside students

--- pages/Register/PhysicalPayment.tsx
+++ pages/Register/PhysicalPayment.tsx
@@ -6,7 +6,10 @@ function PhysicalPayment() {
   return (
     <div className='bg-[#00002C] h-screen'>
         <Navbar/>
-        <h1 className='text-white text-3xl'>EARLY BIRD OFFER OPEN FOR OFFLINE REGISTRATION AT ACAD BLOCK ROOM 204 NOW AND HB1 AND HB2 COMMON AREA FROM 8-11PM TODAY</h1>
+        <div className='p-5'>
+        <h1 className='text-white text-3xl mb-20 mt-10'>NON-MAHE STUDENTS: REGISTRATIONS OPENING SOON!</h1>
+        <h1 className='text-white text-3xl'>MAHE STUDENTS: EARLY BIRD OFFER OPEN FOR OFFLINE REGISTRATION AT COMMON AREA HB1 AND HB2 COMMON AREA FROM 9-12PM TODAY</h1>
+        </div>
     </div>
   )
 }
